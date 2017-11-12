62f40dd1e1cfa91d68fe180b690db3cd4bc17d0a
Guilhermefcasagrande <guilhermefc@unidavi.edu.br>
2017-11-12 11:39:21 -0200

    primeiro commit

---
+++ src/Barco.java
@@ -0,0 +1,31 @@
+public class Barco extends Veiculo implements Acao {
+
+    String volante = "Azul";
+
+    String material = "Madeira";
+    String desloca = "Água";
+
+    private String marca;
+    private String nome;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void ligar(){
+        System.out.println("Ligando....");
+    }
+}
